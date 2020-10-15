8a8b75dc0ab724cd30eb2a04072e35833d97464d
stasyarcev <stasyarcev@gmail.com>
2020-10-15 15:41:11 +0700

    I'm add new swift file

---
+++ Sources/LocalizationHelper/SomethingAnotherHappened.swift
@@ -0,0 +1,18 @@
+//
+//  SomethingAnotherHappened.swift.swift
+//  
+//
+//  Created by Стас Ярцев on 15.10.2020.
+//
+
+import Foundation
+
+
+public struct SomethingAnother {
+    
+    public static func doSomething() {
+        
+        print("Something another happened...")
+    }
+    
+}
