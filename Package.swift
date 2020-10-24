746babd1da45cad4092a47aed1ed75511c172a6c
stasyarcev <stasyarcev@gmail.com>
2020-10-24 17:29:46 +0700

    features complete

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LocalizationHelper",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")],
+            resources: [.process("Dictionary.swift")]
+            ),
         .testTarget(
             name: "LocalizationHelperTests",
             dependencies: ["LocalizationHelper"]),
--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -1,19 +1,68 @@
 
 import Foundation
 
+//math
+var math = false
 
-if CommandLine.arguments.count == 1 {
-    def()
-} else if CommandLine.arguments.count == 3 {
-    if CommandLine.arguments[1] == "-k" && CommandLine.arguments.count == 3 {
-        let word = CommandLine.arguments[2]
-        k(word: word)
-    } else if CommandLine.arguments[1] == "-l" {
-        let word = CommandLine.arguments[2]
-        l(word: word)
+//-l
+func l(lang: String) {
+    for i in 0 ..< dictionary.count {
+        if dictionary[i].values.first?.keys.contains(lang) == true {
+            math = true
+            for (l, v) in dictionary[i].values.first! {
+                if l == lang {
+                    print("\(dictionary[i].keys.first!) : \(v)")
+                }
+            }
+        }
     }
-} else if CommandLine.arguments.count == 5 {
-    let word = CommandLine.arguments[2]
-    let lang = CommandLine.arguments[4]
-    kl(word: word, lang: lang)
+    if math == false {
+        print("Not found")
+    }
 }
+
+//-k
+func k(key: String) {
+    for i in 0 ..< dictionary.count {
+        if dictionary[i].keys.contains(key) == true {
+            math = true
+            print(key)
+            for (lang, val) in dictionary[i].values.first! {
+                print("\(lang): \(val)")
+            }
+        }
+    }
+    if math == false {
+        print("Not found")
+    }
+}
+
+// booth
+func kl(key: String, lang: String) {
+    for i in 0 ..< dictionary.count {
+        if dictionary[i].keys.first!.contains(key) {
+            for (language, value) in dictionary[i].values.first! {
+                if language == lang {
+                    math = true
+                    print(value)
+                }
+            }
+        }
+    }
+    if math == false {
+        print("Not found")
+    }
+}
+
+func def() {
+    for i in 0 ..< dictionary.count {
+        for (word, trans) in dictionary[i] {
+            print(word)
+            for (lang, val) in trans {
+                print("\(lang): \(val)")
+            }
+        }
+    }
+}
+
+Values.main()
