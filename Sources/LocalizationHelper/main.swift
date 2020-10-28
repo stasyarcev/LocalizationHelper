71213d23c938bc2612c1d11416d1cb6720128cab
stasyarcev <stasyarcev@gmail.com>
2020-10-28 18:18:44 +0700

    Добавил search subcommand

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -74,45 +74,73 @@ func def() {
 }
 
 struct Values: ParsableCommand {
-
-    @Option(name: .shortAndLong, help: "Displays all translations of a 'word'.")
-    var key: String?
-    @Option(name: .shortAndLong, help: "Displays all words of a 'language'.")
-    var language: String?
     
-    func run() throws {
-            
-        if key == nil && language == nil {
-            def()
-        } else if let key = key, language == nil {
-            k(key: key)
-        } else if let language = language, key == nil {
-            l(lang: language)
-        } else if let key = key, let language = language {
-            kl(key: key, lang: language)
+    static let configuration = CommandConfiguration(abstract: " Dictionary of words and their translation", subcommands: [Search.self, Update.self, Delete.self])
+    
+}
+
+extension Values {
+    
+    //Для поиска слов теперь использовать конструкцию с ключевым словом search. Работа с ключами остается прежней, они оба опциональные.
+    //> app search -k <key> -l <language>
+    
+    struct Search: ParsableCommand {
+        
+        static let configuration = CommandConfiguration(abstract: "To search for words now use the construction with the search subcommand. Working with keys is the same, they are both optional.")
+        
+        @Option(name: .shortAndLong, help: "Displays all translations of a 'word'.")
+        var key: String?
+        @Option(name: .shortAndLong, help: "Displays all words of a 'language'.")
+        var language: String?
+        
+//        func validate() throws {
+//            guard math == true else {
+//                throw ValidationError("Not Found")
+//            }
+//        }
+        
+        func run() throws {
+                
+            if key == nil && language == nil {
+                def()
+            } else if let key = key, language == nil {
+                k(key: key)
+            } else if let language = language, key == nil {
+                l(lang: language)
+            } else if let key = key, let language = language {
+                kl(key: key, lang: language)
+            }
         }
     }
-}
+    
+    //Для обновления (добавления) используем ключевое слово update. Оба ключа обязательны.
+    //> app update <word> -k <key> -l <language>
 
-//Для поиска слов теперь использовать конструкцию с ключевым словом search. Работа с ключами остается прежней, они оба опциональные.
-//> app search -k <key> -l <language>
+    struct Update: ParsableCommand {
+        
+        static let configuration = CommandConfiguration(abstract: "To update (add) use the update keyword. Both keys are required.")
+        
+        @Option(name: .shortAndLong, help: "-k - name of the key for storing (adding) words")
+        var key: String
+        @Option(name: .shortAndLong, help: "Enter the language in which this 'word' is written.")
+        var language: String
+        
+        func run() {
+            print("Updating JSON file...")
+        }
+    }
 
-struct Search: ParsableCommand {
-    //
-}
+    //Для удаления используем ключевое слово delete. Обязателен один из ключей, могут использоваться оба ключа.
+    //> app delete -k <key> -l <language>
 
-//Для обновления (добавления) используем ключевое слово update. Оба ключа обязательны.
-//> app update <word> -k <key> -l <language>
-
-struct Update: ParsableCommand {
-    //
-}
-
-//Для удаления используем ключевое слово delete. Обязателен один из ключей, могут использоваться оба ключа.
-//> app delete -k <key> -l <language>
-
-struct Delete: ParsableCommand {
-    //
+    struct Delete: ParsableCommand {
+        
+        static let configuration = CommandConfiguration(abstract: "To delete, use the delete keyword. One of the keys is required, both keys can be used.")
+        
+        func run() {
+            print("Deleting JSON file...")
+        }
+    }
 }
 
 Values.main()
